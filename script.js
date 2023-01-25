c173620a182fb7a7e3b5dc94b7d85d108986ae33
Nelson de Barros <nelsinho.527@gmail.com>
2023-01-25 16:13:05 -0300

    password validation

---
+++ script.js
@@ -0,0 +1,11 @@
+function onChange() {
+    const password = document.querySelector('input[name=password]')
+    const confirm = document.querySelector('input[name=confirm-password]')
+
+    if (confirm.value === password.value) {
+        confirm.setCustomValidity('');
+    }
+    else {
+        confirm.setCustomValidity('Passwords do not match');
+    }
+}
